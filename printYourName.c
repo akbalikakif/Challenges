da1aa12883978ec85883a2eb3d5ae5104022a160
Akif Ali Akbalık <111509037+akbalikakif@users.noreply.github.com>
2022-12-13 10:08:43 +0300

    Akif Akbalık
    @themanoftalent

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,20 @@
 // your name surname and age from user
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+  printf("Hello What is your name,surname and age ?");
+  char name[15];
+  char surname[20];
+  int age;
+  scanf("%s",name );
+  scanf("%s",surname);
+  scanf("%d",&age);
+
+printf("Your name is : %s ,your surname is : %s and your age : %d",name,surname,age);
+
+
+
+
+}

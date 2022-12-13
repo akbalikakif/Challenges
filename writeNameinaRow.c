3735db228468cb560ba6c56c96b3ef723e745f82
Akif Ali Akbalık <111509037+akbalikakif@users.noreply.github.com>
2022-12-13 10:43:41 +0300

    Update writeNameinaRow.c
    @themanoftalent

--- writeNameinaRow.c
+++ writeNameinaRow.c
@@ -1,17 +1,18 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 int main(){
- int i,j,k,n; 
+int i,j,k,n;
  printf("Enter the number of the row :");
  scanf("%d",&n);
- 
+
  for(i=0;i<n;i++){
-     for(j=0;i<n;i++){
-     
-     
+
+  for(j=0;j<i;j++)
+{
+   printf("Akif\n");
  }
-     printf("Akif")
+printf("\n");
  }
- 
+
     
 }
